d95392d4e2c4b2298e320da27f487e5811359deb
Mark Medum Bundgaard <mmedum@gmail.com>
2018-12-17 20:15:38 +0100

    health and metrics endpoint added

--- api.py
+++ api.py
@@ -3,8 +3,14 @@ import responder
 api = responder.API()
 
 
+@api.route("/health")
+def health(req, resp):
+    resp.media = {"health": "ok"}
+#    resp.status_code = api.status_codes.HTTP_200
+
+
 @api.route("/")
-async def hello_world(req, resp):
+def hello_world(req, resp):
     resp.text = "Hello World!"
 
 
