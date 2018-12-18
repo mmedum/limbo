a9e3774b00e69a1084559b7270038e2b47bf0780
Mark Medum Bundgaard <mmedum@gmail.com>
2018-12-18 20:25:43 +0100

    test worker to consume from sqs

--- workers/worker.py
+++ workers/worker.py
@@ -1,8 +1,19 @@
 import logging
+import boto3
+import os
 
 
 def main():
     logging.info('Starting up')
+    sqs = boto3.resource('sqs', region_name=os.environ.get('REGION'))
+    queue = sqs.get_queue_by_name(QueueName=os.environ.get('QUEUE'))
+
+    while True:
+        logging.info('Waiting for messages')
+        messages = queue.receive_messages(WaitTimeSeconds=20, MaxNumberOfMessages=10)
+        for message in messages:
+            logging.info(f'{message.body}')
+            message.delete()
 
 
 if __name__ == '__main__':
