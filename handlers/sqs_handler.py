5f632d287af0a92cd062725df2576593f48cef74
Mark Medum Bundgaard <mmedum@gmail.com>
2018-12-18 18:09:56 +0100

    setting region for boto3

--- handlers/sqs_handler.py
+++ handlers/sqs_handler.py
@@ -1,15 +1,13 @@
 import boto3
 import os
 import json
-from sanic.log import logger
 
 
 class SqsHandler:
 
     def __init__(self):
-        self._sqs = boto3.resource('sqs')
+        self._sqs = boto3.resource('sqs', region_name=os.environ.get('REGION'))
         self._queue = self._sqs.get_queue_by_name(QueueName=os.environ.get('QUEUE'))
-        logger.info(self._queue)
 
     def send_message(self, message):
         should_try_sending = True
